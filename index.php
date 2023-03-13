dbd678512413e0db4aaccfcc7092e7647cd05b34
brprotsenko <b.r.protsenko@student.khai.edu>
2023-03-13 15:37:05 +0200

    My second commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+// !!! TODO 1: ваш код обработки GET запроса; выполнения запроса через
+//cURL в поисковую систему; подготовка данных для отрисовки
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h2>My Browser</h2>
+<form method="GET" action="/index.php">
+    <label for="search">Search:</label>
+    <input type="text" id="search" name="search" value=""><br><br>
+    <input type="submit" value="Submit">
+</form>
+<?php
+// !!! TODO 2: ваш код отрисовки ответа
+?>
+</body>
+</html>
